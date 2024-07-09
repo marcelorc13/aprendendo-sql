5fd7dd7544974493f4a40697510a746da41d008a
Marcelo <marcelorcarnauba@gmail.com>
2024-07-09 14:42:44 -0300

    Criando projeto báscio para aprender a introduzir MySQL com Node

---
+++ projeto-com-node/database.js
@@ -0,0 +1,45 @@
+import mysql from 'mysql2'
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '162435mar',
+    database: 'teste'
+}).promise()
+
+//Função para mostrar a tabela
+const [resultado] = await pool.query("SELECT * FROM familia;")
+
+
+//Função para criar tabela
+const criarTabela = async () => {
+    const res = await pool.query(`
+        CREATE TABLE familia(
+        nome VARCHAR(20),
+        sobrenome VARCHAR(40),
+        data_de_nascimento DATE
+        )
+        ;`)
+
+    return res;
+}
+//await criarTabela()
+
+//Função para inserir valores na tabela
+const inserirValores = async () => {
+    const arr = {
+        nome: 'Marcelo',
+        sobrenome: 'Ramalho',
+        data_de_nascimento: '2005-07-13'
+    }
+
+    const res = await pool.query(`
+        INSERT INTO familia(nome, sobrenome, data_de_nascimento)
+        VALUES(?, ?, ?)
+        `, [arr.nome, arr.sobrenome, arr.data_de_nascimento])
+
+    return res
+}
+//await inserirValores()
+
+console.log(resultado)
