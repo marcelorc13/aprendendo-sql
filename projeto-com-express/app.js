4f120531baef74948bb1c263759b6882960ae280
Marcelo <marcelorcarnauba@gmail.com>
2024-07-18 00:42:08 -0300

    Novas funções no projeto com express

--- projeto-com-express/app.js
+++ projeto-com-express/app.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getFamilia } from './database.js'
+import { getFamilia, getMembro, getGastos, getSomaGastos, getTransacao, getTransacaoPorNome } from './database.js'
 const app = express()
 const port = 8080
 
@@ -14,4 +14,31 @@ app.get('/', (req, res) => {
 app.get('/familia', async (req, res) => {
     const familia = await getFamilia()
     res.send(familia)
+})
+
+app.get('/familia/:id', async (req, res) => {
+    const id = req.params.id
+    const membro = await getMembro(id)
+    res.send(membro)
+})
+
+app.get('/gastos', async (req, res) => {
+    const gastos = await getGastos()
+    res.send(gastos)
+})
+
+app.get('/gastos/soma', async (req, res) => {
+    const soma = await getSomaGastos()
+    res.send(soma)
+})
+app.get('/gastos/:id', async (req, res) => {
+    const id = req.params.id
+    const gastos = await getTransacao(id)
+    res.send(gastos)
+})
+
+app.get('/gastos/familia/:id', async (req, res) => {
+    const id = req.params.id
+    const gastos = await getTransacaoPorNome(id)
+    res.send(gastos)
 })
--- projeto-com-express/database.js
+++ projeto-com-express/database.js
@@ -13,3 +13,49 @@ export const getFamilia = async () => {
     const [familia] = await pool.query('SELECT * FROM familia;')
     return familia
 }
+
+export const getMembro = async (id) => {
+    const [membro] = await pool.query(`
+            SELECT *
+            FROM familia
+            WHERE id_familia = ?;
+        `, id)
+    return membro
+}
+
+export const getGastos = async () => {
+    const [gastos] = await pool.query(`
+        SELECT id_transacao, preco, motivo, data_transacao, gastos.id_familia, CONCAT(nome, ' ', sobrenome) AS nome_completo
+        FROM gastos INNER JOIN familia
+        ON gastos.id_familia = familia.id_familia;
+        ;`
+
+    )
+    return gastos
+}
+
+export const getTransacao = async (id) => {
+    const [transacao] = await pool.query(`
+        SELECT * FROM gastos
+        WHERE id_transacao = ?;
+        `, id)
+    return transacao
+}
+
+export const getTransacaoPorNome = async (id) => {
+    const [transacao] = await pool.query(`
+        SELECT * FROM gastos
+        WHERE id_familia = ?;
+        `, id)
+    return transacao
+}
+
+export const getSomaGastos = async (id) => {
+    const [soma] = await pool.query(`
+        SELECT SUM(preco) as 'gasto total'
+        FROM gastos;
+        `, id)
+    return soma
+}
+
+
